18fa30a837c6f8875565686b9752b3d758afe98d
SHsus1122 <tjgurtn1122@naver.com>
2024-04-08 17:34:01 +0900

    직렬화 및 이벤트함수 공부

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,19 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
+    // Start 의 경우 게임이 시작되자마자 무언가를 해야 하는 특정 오브젝트가 있는 경우에 유용합니다.
+    // 하지만, 사용하고 싶을 때만 특정 동작을 수행하는 오브젝트가 있는 경우(예로 벽을 움직이려는 경우)에 사용합니다.
+    // 즉, 누군가 버튼을 누르면 벽에 대해 Awake 메서드가 호출되어 벽이 어떠한 동작을 하게 되는 경우를 말합니다.
+    // 따라서 게임이 시작될 때가 아니라 오브젝트가 동작할 때 생성하려는 특정 값이 있을 경우에 사용하게 됩니다.
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    // LateUpdate 의 경우에는 카메라 위치를 계산할 때 특히 유용하며 플레이어를 따라가는 카메라에는 보통 이를 사용합니다.
+    // 이렇게 하면 카메라가 플레이어의 움직임에 대한 모든 계산이 끝났음을 인식하고 게임이 업데이트 될 때 어디로 이동해야 하는지 파악하게 됩니다.
     void LateUpdate()
     {
         // Offset the camera behind the player by adding to the player's position
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
-    private float turnSpeed = 25.0f;
+    // SerializeField : private 변수를 에디터 창에서도 보이게 해주는 코드
+    // 이는 에디터 상에서는 사용하고 싶지만 다른 클래스에서는 사용하고 싶지 않은 변수를 정의할 때 사용합니다.
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    // FixedUpdate 는 그냥 Update 의 경우에는 카메라 움직임에서 발생하는 문제를 해결하기에 유용합니다.
+    // 예를 들어 움직임에 따라 카메라가 따라오는 속도에 차이가 생기면 버벅이면서 움직이는 듯한 이질감이 들게 됩니다.
+    // 그런데 이 FixedUpdate 를 사용하면 Update 가 호출되기 전에 호출되므로 게임 플레이 시 발생하는 물리를 계산하려고 할 때 작동하게 됩니다.
+    void FixedUpdate()
     {
         // This is where once per frame
         horizontalInput = Input.GetAxis("Horizontal");
